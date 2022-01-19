9785940d83395bdc836c8a0c69dc25d96150caa6
mauriciomutte <mauriciomutte.mm@gmail.com>
2022-01-19 12:52:13 +0000

    Deploying to gh-pages from  @ 8913565d0227e0ffee60bafa122eaec03710a6d3 🚀

--- react/service-worker.js
+++ react/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "precache-manifest.aa3df45c804cbf89a43c65c39635d967.js"
+  "precache-manifest.5ae50bbf2ddfb651a300b1a244e1b1b1.js"
 );
 
 self.addEventListener('message', (event) => {
