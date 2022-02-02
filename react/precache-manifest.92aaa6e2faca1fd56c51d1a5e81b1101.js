30e250dee983ea94f69c2168b699cb1f775d626e
lupy100 <joao.matheus2819@gmail.com>
2022-02-02 13:54:05 +0000

    Deploying to gh-pages from  @ 55a32bd57f4ca2cc4e18269c63f38298204c98e5 🚀

---
+++ react/precache-manifest.92aaa6e2faca1fd56c51d1a5e81b1101.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24aab3e1d76c8fa6f45df8464b125e53",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.ce2df33c0886eb51e014.bundle.js"
+  },
+  {
+    "url": "runtime~main.ce2df33c0886eb51e014.bundle.js"
+  },
+  {
+    "revision": "b483c9d87eebb4fcc5ea",
+    "url": "static/css/main.344ca7af.chunk.css"
+  },
+  {
+    "revision": "277feaa34fb245039023e50d6402cd65",
+    "url": "static/media/jsm-icons.277feaa3.svg"
+  },
+  {
+    "revision": "5160db8b1649705dce968d1dac5f41fd",
+    "url": "static/media/jsm-icons.5160db8b.woff2"
+  },
+  {
+    "revision": "ba788d0685ae7e5a2e3c673c0f62b3c7",
+    "url": "static/media/jsm-icons.ba788d06.eot"
+  },
+  {
+    "revision": "c3e8d5defd3196a58482a9aba9eadd04",
+    "url": "static/media/jsm-icons.c3e8d5de.ttf"
+  },
+  {
+    "revision": "cf59660186a426b81e4b3cd0b4c03721",
+    "url": "static/media/jsm-icons.cf596601.woff"
+  },
+  {
+    "url": "vendors~main.ce2df33c0886eb51e014.bundle.js"
+  },
+  {
+    "url": "vendors~main.ce2df33c0886eb51e014.bundle.js.LICENSE.txt"
+  }
+]);
--- react/service-worker.js
+++ react/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "precache-manifest.9b29158aac92d94763ad36b46ced6413.js"
+  "precache-manifest.92aaa6e2faca1fd56c51d1a5e81b1101.js"
 );
 
 self.addEventListener('message', (event) => {
