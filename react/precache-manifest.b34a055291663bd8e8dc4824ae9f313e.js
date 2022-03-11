8dd808fda47a85b91bf8f139980e4a3159d42e64
tassioFront <47509510+tassioFront@users.noreply.github.com>
2022-03-11 13:42:12 +0000

    Deploying to gh-pages from  @ efcc105a5ebdec65cc60012f420e823566668d7d 🚀

---
+++ react/precache-manifest.b34a055291663bd8e8dc4824ae9f313e.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d86621bbd207914c928b4192d9751d86",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.040a4633c5dc4bff41b9.bundle.js"
+  },
+  {
+    "url": "runtime~main.040a4633c5dc4bff41b9.bundle.js"
+  },
+  {
+    "revision": "5fd8a3461508a18d5d84",
+    "url": "static/css/main.6aff4bb6.chunk.css"
+  },
+  {
+    "revision": "277feaa34fb245039023e50d6402cd65",
+    "url": "static/media/jsm-icons.277feaa3.svg"
+  },
+  {
+    "revision": "d4b72510c749affb68c4533ffe0328ee",
+    "url": "static/media/jsm-icons.d4b72510.woff2"
+  },
+  {
+    "revision": "d5df0837fa6ba337d25af8ac58cff159",
+    "url": "static/media/jsm-icons.d5df0837.ttf"
+  },
+  {
+    "revision": "fcda5aee905f77874a2b7f930eb22f97",
+    "url": "static/media/jsm-icons.fcda5aee.woff"
+  },
+  {
+    "revision": "ffcb9f4f541ca73169da419dd9193937",
+    "url": "static/media/jsm-icons.ffcb9f4f.eot"
+  },
+  {
+    "url": "vendors~main.040a4633c5dc4bff41b9.bundle.js"
+  },
+  {
+    "url": "vendors~main.040a4633c5dc4bff41b9.bundle.js.LICENSE.txt"
+  }
+]);
--- react/service-worker.js
+++ react/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "precache-manifest.4166078d8589b902f32fcd84ffd60b58.js"
+  "precache-manifest.b34a055291663bd8e8dc4824ae9f313e.js"
 );
 
 self.addEventListener('message', (event) => {
