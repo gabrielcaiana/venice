0f8dd3870eccc5bbdc9b63841f292c5aa1149417
felipefialho <hi@felipefialho.com>
2022-03-16 15:17:09 -0300

    release: add new release to gh-pages

---
+++ react/precache-manifest.9cfba927935b119d9dd9a6bbb31aba56.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a06f608d1a17e6acfabc054b8e5c83f1",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.cf51da1a53e1d7458b03.bundle.js"
+  },
+  {
+    "url": "runtime~main.cf51da1a53e1d7458b03.bundle.js"
+  },
+  {
+    "revision": "a85070466767d9c2fb82",
+    "url": "static/css/main.ed6696da.chunk.css"
+  },
+  {
+    "revision": "277feaa34fb245039023e50d6402cd65",
+    "url": "static/media/jsm-icons.277feaa3.svg"
+  },
+  {
+    "revision": "3d0463e7c293d2bcb9cd3a0911ade6de",
+    "url": "static/media/jsm-icons.3d0463e7.woff2"
+  },
+  {
+    "revision": "af60ee2470b5d0745d3cd91d33d4ebba",
+    "url": "static/media/jsm-icons.af60ee24.ttf"
+  },
+  {
+    "revision": "b2b098819933627abee815913c7af73f",
+    "url": "static/media/jsm-icons.b2b09881.eot"
+  },
+  {
+    "revision": "e38e42785a0df79a547ec8e70af8790f",
+    "url": "static/media/jsm-icons.e38e4278.woff"
+  },
+  {
+    "url": "vendors~main.cf51da1a53e1d7458b03.bundle.js"
+  },
+  {
+    "url": "vendors~main.cf51da1a53e1d7458b03.bundle.js.LICENSE.txt"
+  }
+]);
--- react/service-worker.js
+++ react/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "precache-manifest.c7fced5f22990e4b94b859595907a3a7.js"
+  "precache-manifest.9cfba927935b119d9dd9a6bbb31aba56.js"
 );
 
 self.addEventListener('message', (event) => {
