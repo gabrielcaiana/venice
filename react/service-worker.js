f858ada29dd6b1f73fc29c05688cc8e605eb9ee1
munyrasirio <munyrasirio@gmail.com>
2022-05-12 17:46:54 +0000

    Deploying to gh-pages from @ juntossomosmais/venice@4caf8f99662f7ede45d8da84894d8c3d0bc1ecf4 🚀

--- react/service-worker.js
+++ react/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "precache-manifest.97ea69d1ff1b266e5576c162220acf45.js"
+  "precache-manifest.f44efbdeaa077564459b439ece2c9d40.js"
 );
 
 self.addEventListener('message', (event) => {
